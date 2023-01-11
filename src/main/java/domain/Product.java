4d1f8b969ae0f873259311f16cacef2c51e92583
seogineer <dgseo8981@gmail.com>
2023-01-11 18:22:36 +0900

    feat: part4 실습 완료

--- src/main/java/domain/Product.java
+++ src/main/java/domain/Product.java
@@ -25,7 +25,14 @@ public class Product {
         return stock < 5;
     }
 
+    public boolean isPriceUnderMinimum() {
+        return price < 1000;
+    }
+
     public void minusStock() {
+        if (isSoldOut() && isPriceUnderMinimum()) {
+            throw new RuntimeException("주문할 수 없는 상품입니다.");
+        }
         stock -= 1;
     }
 
